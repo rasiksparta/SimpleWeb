d36e957361bfd8352341939e62578013698589bf
Rasik Rana <RRana@spartaglobal.com>
2019-09-17 10:04:29 +0100

    version 1.0 <Testing html, css and js>

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("SimpleWebsite")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("SimpleWebsite")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ae870fd0-c1ff-47c2-9c4c-afc50e3af2d5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿var menuicon = document.getElementById("side_menu_icon");
+var menuOpen = false;
+
+function menuControl() {
+    menuOpen = !menuOpen;
+    if (menuOpen) {
+        document.getElementById("side_menu").style.display = "inline-block";
+        document.getElementById("body").style.width = "80%";
+        return;
+    }
+    document.getElementById("side_menu").style.display = "none";
+    document.getElementById("body").style.width = "100%";
+}
